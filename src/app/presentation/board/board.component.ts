099d36825d2102cfa0ef8695564a9a08b064905f
bhuvanarajamani <bhuvana.cr@gmail.com>
2017-05-22 22:48:41 -0400

    UI layout

--- src/app/presentation/board/board.component.ts
+++ src/app/presentation/board/board.component.ts
@@ -6,12 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
-  pits = Array(12).fill(null);
+  pits = Array(12).fill(4);
+  mancala1 = 0;
+  mancala2 = 0;
+
   player = '1';
   winner = null;
 
   get gameStatusMessage(){
     return this.winner? `Player ${this.winner} has won!` : 
     `Player ${this.player}'s turn`;
+  }
+
+  handleMove(position){
+    console.log("clicked ==> ", position);
+    if(!this.winner && !this.pits[position] ){
+      //this.pits[position] = this.player;
+      this.fillPits(position);
+      if(this.winnigMove()) {
+        this.winner = this.player;
+      }
+      this.player = this.player === '1' ? '2' : '1';
+    }
+  }
+
+  fillPits(startpos: number) {
+    
+  }
+
+  winnigMove() {
+    
   }
 }
